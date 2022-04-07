0b8a4f6b3b2db17dd41cdb1e6e4b50c8e81e340f
Martyna Stanislawska <martynastan@gmail.com>
2022-04-07 14:36:58 +0100

    employee select functional, employeeDetail component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {useState,useEffect} from 'react';
 import './App.css';
-// import EmployeeSelector from './EmployeeSelector';
+import SelectedEmployeeDetail from './SelectedEmployeeDetail';
 
 
   function App() {
     const [employees,setEmployees]=useState([]);
+    const[selectedEmployee,setSelectedEmployee] = useState(null)
+
     const getData=()=>{
       fetch('cardData.json'
       ,{
@@ -27,14 +29,24 @@ import './App.css';
       getData()
     },[])
 
+    const onEmployeeSelected=(employee)=>{
+      setSelectedEmployee(employee)
+    }
+
+    const handleChange= (event)=>{
+      onEmployeeSelected(employees[event.target.value])
+
+    }
+
     
 
   return (
     <div className="App">
-      <select defaultValue="">
+      <select onChange ={handleChange} defaultValue="">
       <option value="">Choose an Employee</option>
       {employees.map((employee,index)=>{return <option value={index} key={index}>{employee.employeeName}</option>})}
       </select>
+      {selectedEmployee ? <SelectedEmployeeDetail selectedEmployee={selectedEmployee}/>:null}
     </div>
   );
 }
---
+++ src/SelectedEmployeeDetail.js
@@ -0,0 +1,11 @@
+const SelectedEmployeeDetail=({selectedEmployee})=>{
+    console.log(selectedEmployee.moreInfo)
+    return(
+        <div>
+            <h2>{selectedEmployee.employeeName}</h2>
+            whtever
+        </div>
+    )
+}
+
+export default SelectedEmployeeDetail;

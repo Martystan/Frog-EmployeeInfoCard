021c0c164bd113b656466de52e2d9434719c76a0
Martyna Stanislawska <martynastan@gmail.com>
2022-04-07 16:28:14 +0100

    more Info button funtional

--- src/SelectedEmployeeDetail.js
+++ src/SelectedEmployeeDetail.js
@@ -1,11 +1,30 @@
+import {useState} from 'react'
+
 const SelectedEmployeeDetail=({selectedEmployee})=>{
-    console.log(selectedEmployee.moreInfo)
-    return(
+    const [visible, setVisible] = useState(false)
+    const handleClick= () =>{setVisible(!visible)}
+    if (visible) {return(
         <div>
             <h2>{selectedEmployee.employeeName}</h2>
             <img src={require("" + selectedEmployee.picture) } alt="employee"></img>
+            <p>{selectedEmployee.jobTitle}</p>
+            <p>{selectedEmployee.companyName}</p>
+            <button onClick={handleClick}>Hide Info</button>
+            <p> {selectedEmployee.moreInfo}</p>
         </div>
-    )
+    )}
+    else {
+        return(
+            <div>
+            <h2>{selectedEmployee.employeeName}</h2>
+            <img src={require("" + selectedEmployee.picture) } alt="employee"></img>
+            <p>{selectedEmployee.jobTitle}</p>
+            <p>{selectedEmployee.companyName}</p>
+            <button onClick={handleClick}>More Info</button>
+            
+        </div>
+        )
+    }
 }
 
 export default SelectedEmployeeDetail;
